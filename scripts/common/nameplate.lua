8a82b53fe302950953640ac80ba4336215e522b2
TheBunnyMan123 <bunny@tkbunny.net>
2025-06-21 12:52:02 -0500

    nameplate

--- scripts/common/nameplate.lua
+++ scripts/common/nameplate.lua
@@ -1,25 +1,58 @@
-nameplate.ENTITY:setOutline(true)
+nameplate.ENTITY:setVisible(true)
 
-function events.TICK()
-   local json = {
-      {
-         text = "${badges}"
-      },
-      {
-         text = "\nTheKillerBunny",
+local json = {
+   {
+      text = "${badges}"
+   },
+   {
+      text = "\nTheKillerBunny",
+      color = "#32FF96"
+   }
+}
+
+nameplate.ALL:setText(toJson(json))
+
+local nameText = "\xE2\x9C\xA8TheKillerBunny\xE2\x9C\xA8"
+local name = {}
+
+for char in string.gmatch(nameText, "([%z\1-\127\128-\255][\128-\191]*)") do
+   name[#name + 1] = char
+end
+
+local entityTasks = {}
+local width = 0
+local holder = models.models.model:newPart("NAMEPLATE", "CAMERA")
+for i = 1, #name do
+   entityTasks[#entityTasks+1] = {
+      char = name[i],
+      task = holder:newText("task" .. i):setText(toJson {
+         text = name[i],
          color = "#32FF96"
-      }
+      }):setOutline(true):setPos(-width, 0):setScale(3/8):setLight(15),
+      offset = i,
+      prevWidth = width
    }
 
-   nameplate.ALL:setText(toJson(json))
+   width = width + client.getTextWidth(name[i] or "") * (3/8)
+end
 
-   if avatar:getMaxParticles() < 2000 then
-      json[#json + 1] = {
-         text = "\nPlease raise my particle limit",
-         color = "red"
-      }
+for _, v in pairs(entityTasks) do
+   v.task:setPos(v.task:getPos().x + width / 2, 0)
+end
+holder:setPivot(0, 40, 0)
 
-      nameplate.ENTITY:setText(toJson(json))
+local tick = 0
+function events.TICK()
+   tick = tick + 1
+end
+
+function events.RENDER(delta)
+   for _, v in pairs(entityTasks) do
+      v.task:setPos(-v.prevWidth + width / 2, math.lerp(
+         math.sin((tick + v.offset - 1) / 4),
+         math.sin((tick + v.offset) / 4),
+         delta
+      ))
    end
 end
 
