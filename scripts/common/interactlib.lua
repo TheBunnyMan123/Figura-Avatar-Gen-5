ba92a1968141a2ae6900f90f8341c3b6f69d67f6
TheBunnyMan123 <bunny@tkbunny.net>
2026-01-01 22:01:01 -0600

    orgaanize

--- scripts/common/interactlib.lua
+++ scripts/common/interactlib.lua
@@ -1,12 +1,37 @@
-local action = wheel:newAction():setTitle("Player Mover [RMB to throw]"):setItem("fishing_rod"):setOnToggle(function() end)
+local ctrl = keybinds:newKeybind("ctrl", "key.keyboard.left.control")
+local shift = keybinds:newKeybind("shift", "key.keyboard.left.shift")
+
+local int = require("libs.playerInt.setup")
+int.funcs.immune = true
+int.config.speedLimit = 10
+
+local page = action_wheel:newPage("interaction")
+wheel:newAction():setTitle("Interaction Lib"):setItem("player_head")
+	:setOnLeftClick(function()
+		action_wheel:setPage(page)
+	end)
+
+page:newAction():setTitle("Back"):setItem("arrow")
+	:setOnLeftClick(function()
+		action_wheel:setPage(wheel)
+	end)
+
+page:newAction():setTitle("Velocity Limit [10]"):setItem("feather")
+	:setOnScroll(function(dir, self)
+		int.config.speedLimit = int.config.speedLimit + ((dir > 0) and 1 or -1) * (ctrl:isPressed() and 5 or 1) * (shift:isPressed() and 10 or 1)
+		self:setTitle(string.format("Velocity Limit [%d]", int.config.speedLimit))
+	end)
+page:newAction():setTitle("Disabled"):setItem("lever"):setOnToggle(function(enabled, self)
+	int.funcs.immune = not enabled
+end):setToggleTitle("Enabled")
+
+local action = page:newAction():setTitle("Player Mover [RMB to throw]"):setItem("fishing_rod"):setOnToggle(function() end)
 local click = keybinds:newKeybind("move", "key.mouse.left")
 local throw = keybinds:newKeybind("throw", "key.mouse.right")
 local movelib = require("libs.playerInt.picker")
 
 local throw_strength = 10
-local ctrl = keybinds:newKeybind("ctrl", "key.keyboard.left.control")
-local shift = keybinds:newKeybind("shift", "key.keyboard.left.shift")
-wheel:newAction():setTitle("Throw Strength [10]"):setItem("firework_rocket")
+page:newAction():setTitle("Throw Strength [10]"):setItem("firework_rocket")
 	:setOnScroll(function(dir, self)
 		throw_strength = throw_strength + ((dir > 0) and 1 or -1) * (ctrl:isPressed() and 5 or 1) * (shift:isPressed() and 10 or 1)
 		self:setTitle(string.format("Throw Strength [%d]", throw_strength))
