3f03514847587c53c214a6ead8a14108596aaab8
TheBunnyMan123 <bunny@tkbunny.net>
2026-01-03 21:27:49 -0600

    commands

---
+++ scripts/host/commands.lua
@@ -0,0 +1,35 @@
+local commands
+commands = {
+	help = function(full)
+		local json = {}
+		for k in pairs(commands) do
+			json[#json + 1] = {
+				{
+					text = "!" .. k,
+					color = "green"
+				},
+				{
+					text = ", ",
+					color = "gray"
+				}
+			}
+		end
+		printJson(toJson(json))
+	end,
+	testzone = function(full)
+		host:sendChatCommand("tp @s 123980 65 198872 0 0")
+	end
+}
+
+function events.CHAT_SEND_MESSAGE(msg)
+	for cmd, func in pairs(commands) do
+		if msg:match("^!" .. cmd .. " ") or  msg:match("^!" .. cmd .. "$") then
+			func(msg)
+			host:appendChatHistory(msg)
+			return ""
+		end
+	end
+
+	return msg
+end
+
